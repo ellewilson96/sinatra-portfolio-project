4b3e281adaf2c4cf7cdb801172ea47ceb197e53d
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-24 00:14:58 +0000

    Created signup page with working submission form

--- skybucks/app/controllers/user_controller.rb
+++ skybucks/app/controllers/user_controller.rb
@@ -1,2 +1,11 @@
 class UserController < ApplicationController
+
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    @user.save
+  end
 end
