462c7206607cd89aa167ee08909f7840a75fa29c
Elle Wilson <eleanorwilsonn@gmail.com>
2018-06-25 23:23:05 +0000

    Defined helper methods, flight controller routes.

--- skybucks/app/controllers/flight_controller.rb
+++ skybucks/app/controllers/flight_controller.rb
@@ -1,7 +1,90 @@
 class FlightController < ApplicationController
 
-  get '/flights/new' do
-    erb :'/flights/create_flight'
-  end
+  if logged_in?
+        @flights = Flight.all
+        erb :'flights/flights'
+      else
+        redirect to '/login'
+      end
+    end
 
+    get '/flights/new' do
+      if logged_in?
+        erb :'flights/create_tweet'
+      else
+        redirect to '/login'
+      end
+    end
+
+    post '/flights' do
+     if logged_in?
+       if params == ""
+         redirect to "/tweets/new"
+       else
+         @flight = current_user.flights.build(params)
+         if @flight.save
+           redirect to "/flights/#{@flight.id}"
+         else
+           redirect to "/flights/new"
+         end
+       end
+     else
+       redirect to '/login'
+     end
+   end
+
+   get '/flights/:id' do
+     if logged_in?
+       @flight = Flight.find_by_id(params[:id])
+       erb :'flights/show_flight'
+     else
+       redirect to '/login'
+     end
+   end
+
+   get '/flights/:id/edit' do
+     if logged_in?
+       @flight = Flight.find_by_id(params[:id])
+       if @flight && @flight.user == current_user
+         erb :'flights/edit_tweet'
+       else
+         redirect to '/flights'
+       end
+     else
+       redirect to '/login'
+     end
+   end
+
+   patch '/flights/:id' do
+     if logged_in?
+       if params == ""
+         redirect to "/flights/#{params[:id]}/edit"
+       else
+         @flight = Flight.find_by_id(params[:id])
+         if @flight && @flight.user == current_user
+           if @flight.update(params)
+             redirect to "/flights/#{@flight.id}"
+           else
+             redirect to "/flights/#{@flight.id}/edit"
+           end
+         else
+           redirect to '/flights'
+         end
+       end
+     else
+       redirect to '/login'
+     end
+   end
+
+   delete '/flights/:id/delete' do
+     if logged_in?
+       @flight = Flight.find_by_id(params[:id])
+       if @flight && @flight.user == current_user
+         @flight.delete
+       end
+       redirect to '/flights'
+     else
+       redirect to '/login'
+     end
+   end
 end
